03d51770a3714ed039f03329e77ba898cfdc1a75
Swetha2225 <167203590+Swetha2225@users.noreply.github.com>
2026-02-19 10:16:10 +0530

    Time: 4 ms (25.47%) | Memory: 8.1 MB (45.65%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isPalindrome(int x) {
+   if (x < 0)
+    return false;
+    int original = x;
+    long reverse = 0;
+   while (x > 0) 
+   {
+    int digit = x % 10;
+    reverse = reverse * 10 + digit;
+    x = x / 10;             
+   }
+
+    if (original == reverse)
+    return true;
+    else
+    return false;
+}
