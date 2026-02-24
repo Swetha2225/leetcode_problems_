8c8ed8558dd3346c5628cc862367b4cf659a8901
Swetha2225 <167203590+Swetha2225@users.noreply.github.com>
2026-02-24 20:56:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (55.37%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0) return "";
+
+    int i, j;
+
+    for (i = 0; strs[0][i] != '\0'; i++) {
+
+        for (j = 1; j < strsSize; j++) {
+
+            if (strs[j][i] == '\0' || strs[j][i] != strs[0][i]) {
+                strs[0][i] = '\0';
+                return strs[0];
+            }
+        }
+    }
+
+    return strs[0];
+}
